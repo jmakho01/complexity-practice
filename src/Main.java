db0e9c13f1e8e5f442211cf287603941c562bb61
jmakho01 <210610870+jmakho01@users.noreply.github.com>
2025-10-16 14:31:00 -0700

    Added answer for question 1

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import java.util.Set;
 public class Main {
 
   // The time complexity is:
-  // YOUR ANSWER HERE
+  // O(x^2)
   public static void timesTable(int x) {
     for(int i = 1; i <= x; i++) {
         for(int j = 1; j <= x; j++) {
